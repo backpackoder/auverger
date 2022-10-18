4f4f4dad770db3cd8c4d72c0074f2494e13906ca
harderth <th.barbiera@outlook.fr>
2022-10-17 21:05:27 -0500

    Mon 1er commit au verger

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/header';
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      {/* <Menu />
+      <Carousel />
+      <Part /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/header.js
@@ -0,0 +1,9 @@
+import './header.css';
+
+function Header() {
+    return (
+        <h1 className="header">Au verger B&B</h1>
+    )
+}
+
+export default Header
