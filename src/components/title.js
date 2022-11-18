bf08095856e5c96d6f40253e8dcb0c37d2df2b3f
harderth <th.barbiera@outlook.fr>
2022-11-17 19:58:30 -0600

    eso

--- src/components/title.js
+++ src/components/title.js
@@ -1,5 +1,11 @@
 function Title() {
-    return <h1 id="h1" translate="no">Au verger B&B</h1>
+    return (
+        <>
+        <div id="topHeader">
+            <h1 id="h1" translate="no">Au verger B&B</h1>
+        </div>
+        </>
+    )
 }
 
 export default Title
